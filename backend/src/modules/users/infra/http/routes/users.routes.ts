20cbfeea7f469c5c2514a447c769be606e430ac8
gabrielrportugal <gabrielrportugal@outlook.com>
2020-05-20 17:35:20 -0300

    Adicionando cache na aplicação e upload de arquivos para o Amazon SES pronto

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import UserAvatarController from '../controllers/UserAvatarController';
 const usersRouter = Router();
 const userAvatarController = new UserAvatarController();
 const usersController = new UsersController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRouter.post(
   '/',
